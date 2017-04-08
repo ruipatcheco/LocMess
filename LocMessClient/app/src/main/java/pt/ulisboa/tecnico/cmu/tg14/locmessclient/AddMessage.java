1c7ca6236c8945a5bf3f2f12e908a00e4fea9312
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-04-08 13:45:59 +0100

    Added generic listener for date picker

--- LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/AddMessage.java
+++ LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/AddMessage.java
@@ -2,13 +2,67 @@ package pt.ulisboa.tecnico.cmu.tg14.locmessclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.security.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import pt.ulisboa.tecnico.cmu.tg14.locmessclient.Listeners.DateTimeListener;
 
 public class AddMessage extends AppCompatActivity {
+
+    private final String TAG = "AddMessage";
+    private EditText mMessageContent;
+    private EditText mStartTime;
+    private EditText mEndTime;
+    private Calendar mCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_message);
         setTitle(R.string.title_activity_add_message);
+        mCalendar = Calendar.getInstance();
+
+        mMessageContent = (EditText) findViewById(R.id.add_message_content);
+        mStartTime = (EditText) findViewById(R.id.add_message_start_time);
+        mEndTime = (EditText) findViewById(R.id.add_message_end_time);
+
+        mStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DateTimeListener dateTimeListener = new DateTimeListener(getFragmentManager(),mStartTime);
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(dateTimeListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show(getFragmentManager(),"DateTimePickerDialog");
+
+            }
+        });
+
+        mEndTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateTimeListener dateTimeListener = new DateTimeListener(getFragmentManager(),mEndTime);
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(dateTimeListener,
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show(getFragmentManager(),"DateTimePickerDialog");
+
+            }
+        });
+
     }
 }
---
+++ LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/Listeners/DateTimeListener.java
@@ -0,0 +1,46 @@
+package pt.ulisboa.tecnico.cmu.tg14.locmessclient.Listeners;
+
+import android.app.FragmentManager;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by trosado on 07/04/17.
+ */
+
+public class DateTimeListener implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
+    private EditText mEditText;
+    private FragmentManager mfragmentManager;
+    public DateTimeListener(FragmentManager fragmentManager,EditText mEditText) {
+        this.mEditText = mEditText;
+        this.mfragmentManager=fragmentManager;
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        String str = year+"-"+monthOfYear+"-"+dayOfMonth;
+        TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(DateTimeListener.this,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),true);
+        timePickerDialog.show(mfragmentManager,"TimePickerDialog");
+        mEditText.setText(str);
+        Log.d(TAG,str);
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+        String str = hourOfDay+":"+minute;
+        String date = mEditText.getText().toString();
+        mEditText.setText(date+" "+str);
+        Log.d(TAG,str);
+
+    }
+}
