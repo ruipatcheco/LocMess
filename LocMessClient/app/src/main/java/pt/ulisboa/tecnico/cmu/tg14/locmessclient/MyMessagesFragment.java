f52dba1870c6a72298bfed6c3b6b8402ea754d92
Rui Pacheco <ruipatcheco@outlook.pt>
2017-04-15 17:14:24 +0100

    no message

--- LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/MyMessagesFragment.java
+++ LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/MyMessagesFragment.java
@@ -9,6 +9,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -59,7 +64,6 @@ public class MyMessagesFragment extends Fragment {
 
         getActivity().setTitle(R.string.fragment_my_messages_title);
 
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -77,8 +81,21 @@ public class MyMessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_messages, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_messages, container, false);
+        List<String> list = new ArrayList<>();
+        list.add("MyMess1");
+        list.add("MyMess2");
+        list.add("MyMess3");
+        list.add("MyMess4");
+
+        ListView listView = (ListView) view.findViewById(R.id.list_messages_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,list);
+
+        listView.setAdapter(arrayAdapter);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
