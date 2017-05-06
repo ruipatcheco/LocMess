cce3d8f41e9b2e139f2cff90b7c7d27f1643dbfe
Duarte Botelho <duartebotelho@ist.utl.pt>
2017-05-06 12:53:53 +0100

    Refactored code closes #4

--- LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/Utils/FeedReaderDbHelper.java
+++ LocMessClient/app/src/main/java/pt/ulisboa/tecnico/cmu/tg14/locmessclient/Utils/FeedReaderDbHelper.java
@@ -61,26 +61,26 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
                     FeedEntry.MESSAGE_COLUMN_LOCATION +" "+ FeedEntry.TEXT_TYPE +
                 " )";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.LOCATION_TABLE_NAME;
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ";
 
     public FeedReaderDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-    public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(SQL_CREATE_LOCATION);
+    public void onCreate(SQLiteDatabase db) {
+        createLocationTable(db);
         db.execSQL(SQL_CREATE_MESSAGE);
         db.execSQL(SQL_CREATE_MULE);
     }
+
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
+        onDrop(db);
     }
 
     public void onDrop(SQLiteDatabase db) {
+        dropLocation(db);
         db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
@@ -88,6 +88,108 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         onUpgrade(db, oldVersion, newVersion);
     }
+
+    // LOCATION
+
+    public void createLocationTable(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_LOCATION);
+    }
+
+    public void dropLocation(SQLiteDatabase db) {
+        db.execSQL(SQL_DELETE_ENTRIES + FeedEntry.LOCATION_TABLE_NAME);
+    }
+
+    public void insertLocation (String name, String ssid, String ble, float lat, float lon) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(FeedEntry.LOCATION_COLUMN_NAME, name);
+        contentValues.put(FeedEntry.LOCATION_COLUMN_SSID, ssid);
+        contentValues.put(FeedEntry.LOCATION_COLUMN_BLE, ble);
+        contentValues.put(FeedEntry.LOCATION_COLUMN_LAT, lat);
+        contentValues.put(FeedEntry.LOCATION_COLUMN_LON, lon);
+        db.insert(FeedEntry.LOCATION_TABLE_NAME, null, contentValues);
+        Log.d("insertAllLocations: ","added to DB location " + name + ssid + ble + lat + lon);
+    }
+
+    public void insertAllLocations(List<Location> locations){
+        for (Location l:locations ) {
+            insertLocation(l.getName(), l.getSsid(), l.getBle(),l.getLatitude(), l.getLongitude());
+        }
+    }
+
+    public ArrayList<Location> getAllLocations() {
+        ArrayList<Location> locations = new ArrayList<Location>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = {
+                FeedEntry._ID,
+                FeedEntry.LOCATION_COLUMN_NAME,
+                FeedEntry.LOCATION_COLUMN_SSID,
+                FeedEntry.LOCATION_COLUMN_BLE,
+                FeedEntry.LOCATION_COLUMN_LAT,
+                FeedEntry.LOCATION_COLUMN_LON
+        };
+
+        String sortOrder = FeedEntry.LOCATION_COLUMN_NAME + " ASC";
+
+        Cursor cursor = db.query(
+                FeedEntry.LOCATION_TABLE_NAME,            // The table to query
+                projection,                               // The columns to return
+                null,                                     // The columns for the WHERE clause
+                null,                                     // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        cursor.moveToFirst();
+
+        while(cursor.isAfterLast() == false){
+            Location location = new Location(
+                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_SSID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_BLE)),
+                    cursor.getFloat(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_LAT)),
+                    cursor.getFloat(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_LON))
+            );
+            locations.add(location);
+            cursor.moveToNext();
+        }
+        return locations;
+    }
+
+    public ArrayList<String> getAllLocationsNames() {
+        ArrayList<String> locations = new ArrayList<String>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = {
+                FeedEntry._ID,
+                FeedEntry.LOCATION_COLUMN_NAME,
+        };
+        String sortOrder = FeedEntry.LOCATION_COLUMN_NAME + " ASC";
+
+        Cursor cursor = db.query(
+                FeedEntry.LOCATION_TABLE_NAME,            // The table to query
+                projection,                               // The columns to return
+                null,                                     // The columns for the WHERE clause
+                null,                                     // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        cursor.moveToFirst();
+
+        while(cursor.isAfterLast() == false){
+
+            locations.add(cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_NAME)));
+            cursor.moveToNext();
+        }
+        return locations;
+    }
+
+    // MULE
 
     public void insertMessageMule (long creationTime, long startTime, long endTime, String content, String publisher, String location) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -106,8 +208,6 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
 
     public ArrayList<Message> getAllMuleMessages() {
         ArrayList<Message> messages = new ArrayList<Message>();
-
-        //hp = new HashMap();
         SQLiteDatabase db = this.getReadableDatabase();
 
         String[] projection = {
@@ -120,13 +220,6 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
                 FeedEntry.MULE_COLUMN_LOCATION
         };
 
-        // Filter results WHERE "title" = 'My Title'
-        //String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
-        //String[] selectionArgs = { "My Title" };
-
-        // How you want the results sorted in the resulting Cursor
-        //String sortOrder = FeedEntry.MESSAGE_COLUMN_LOCATION + " DESC";
-
         Cursor cursor = db.query(
                 FeedEntry.MULE_TABLE_NAME,            // The table to query
                 projection,                               // The columns to return
@@ -138,7 +231,6 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
         );
 
         cursor.moveToFirst();
-
 
         while(cursor.isAfterLast() == false){
             Message m = new Message(
@@ -156,7 +248,7 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
         return messages;
     }
 
-
+    // MESSAGE
 
     public void insertMessage (long creationTime, long startTime, long endTime, String content, String publisher, String location) {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -225,107 +317,4 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
         return messages;
     }
 
-    public void insertLocation (String name, String ssid, String ble, float lat, float lon) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(FeedEntry.LOCATION_COLUMN_NAME, name);
-        contentValues.put(FeedEntry.LOCATION_COLUMN_SSID, ssid);
-        contentValues.put(FeedEntry.LOCATION_COLUMN_BLE, ble);
-        contentValues.put(FeedEntry.LOCATION_COLUMN_LAT, lat);
-        contentValues.put(FeedEntry.LOCATION_COLUMN_LON, lon);
-        db.insert(FeedEntry.LOCATION_TABLE_NAME, null, contentValues);
-    }
-
-    public void insertAllLocations(List<Location> locations){
-        for (Location l:locations ) {
-            insertLocation(l.getName(), l.getSsid(), l.getBle(),l.getLatitude(), l.getLongitude());
-            Log.d("insertAllLocations: ","added to DB location" + l.getName());
-        }
-    }
-
-    public ArrayList<Location> getAllLocations() {
-        ArrayList<Location> locations = new ArrayList<Location>();
-
-        //hp = new HashMap();
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        String[] projection = {
-            FeedEntry._ID,
-            FeedEntry.LOCATION_COLUMN_NAME,
-            FeedEntry.LOCATION_COLUMN_SSID,
-            FeedEntry.LOCATION_COLUMN_BLE,
-            FeedEntry.LOCATION_COLUMN_LAT,
-            FeedEntry.LOCATION_COLUMN_LON
-        };
-
-        // Filter results WHERE "title" = 'My Title'
-        //String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
-        //String[] selectionArgs = { "My Title" };
-
-        // How you want the results sorted in the resulting Cursor
-        String sortOrder = FeedEntry.LOCATION_COLUMN_NAME + " DESC";
-
-        Cursor cursor = db.query(
-                FeedEntry.LOCATION_TABLE_NAME,            // The table to query
-                projection,                               // The columns to return
-                null,                                     // The columns for the WHERE clause
-                null,                                     // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-
-        cursor.moveToFirst();
-
-        while(cursor.isAfterLast() == false){
-            Location location = new Location(
-                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_NAME)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_SSID)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_BLE)),
-                    cursor.getFloat(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_LAT)),
-                    cursor.getFloat(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_LON))
-            );
-            locations.add(location);
-            cursor.moveToNext();
-        }
-        return locations;
-    }
-
-    public ArrayList<String> getAllLocationsNames() {
-        ArrayList<String> locations = new ArrayList<String>();
-
-        //hp = new HashMap();
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        String[] projection = {
-                FeedEntry._ID,
-                FeedEntry.LOCATION_COLUMN_NAME,
-        };
-
-        // Filter results WHERE "title" = 'My Title'
-        //String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
-        //String[] selectionArgs = { "My Title" };
-
-        // How you want the results sorted in the resulting Cursor
-        String sortOrder = FeedEntry.LOCATION_COLUMN_NAME + " ASC";
-
-        Cursor cursor = db.query(
-                FeedEntry.LOCATION_TABLE_NAME,            // The table to query
-                projection,                               // The columns to return
-                null,                                     // The columns for the WHERE clause
-                null,                                     // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-
-        cursor.moveToFirst();
-
-        while(cursor.isAfterLast() == false){
-
-            locations.add(cursor.getString(cursor.getColumnIndexOrThrow(FeedEntry.LOCATION_COLUMN_NAME)));
-            cursor.moveToNext();
-        }
-        return locations;
-    }
 }
